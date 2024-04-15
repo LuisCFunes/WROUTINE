09ccb70887bef7e8df2c349e0080ce731fce42c6
LuisCF <luiscfunes521@gmail.com>
2024-04-14 23:38:01 -0600

    inputs for rest and each time set

--- src/components/ui/Btn.tsx
+++ src/components/ui/Btn.tsx
@@ -9,7 +9,7 @@ const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
   return (
     <button
       type="button"
-      className="w-24 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5  dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      className="w-32 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5  dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
       onClick={onClick}
     >
       {children}
--- src/components/ui/inputTimeTotal.tsx
+++ src/components/ui/inputTimeTotal.tsx
@@ -2,116 +2,158 @@ import React, { useState, useEffect } from "react";
 import Btn from "./Btn";
 
 const InputTimeTotal: React.FC = () => {
-    const [minutes, setMinutes] = useState(10); // State to manage the minutes
-    const [seconds, setSeconds] = useState(0); // State to manage the seconds
-    const [isConfirmed, setIsConfirmed] = useState(false); // State to track if time is confirmed
-    const [isRunning, setIsRunning] = useState(false); // State to track if timer is running
+    const [time, setTime] = useState({ minutes: 10, seconds: 0 });
+    const [timeRoutine, setTimeRoutine] = useState({ exerciseTime: 0, restTime: 0, });
+    const [confirms, setIsconfirms] = useState({ confirmRoutine: false, confirmEachSet: false,confirmRest:false });
+    const [isRunning, setIsRunning] = useState(false);
 
-    const handleMinutesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!isConfirmed) {
-            const value = parseInt(e.target.value);
-            setMinutes(isNaN(value) ? 0 : value);
+    const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!confirms.confirmRoutine) {
+            const { value } = e.target;
+            const parsedValue = parseInt(value);
+            setTime((prevTime) => ({
+                ...prevTime,
+                [e.target.name]: isNaN(parsedValue) ? 0 : parsedValue,
+            }));
         }
     };
 
-    const handleSecondsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!isConfirmed) {
-            const value = parseInt(e.target.value);
-            setSeconds(isNaN(value) ? 0 : value);
+    const handleTimeRoutineChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (confirms.confirmRoutine) {
+            const { value } = e.target;
+            const parsedValue = parseInt(value);
+            setTimeRoutine((prevTime) => ({
+                ...prevTime,
+                [e.target.name]: isNaN(parsedValue) ? 0 : parsedValue,
+            }));
         }
     };
 
     const handleConfirmClick = () => {
-        setIsConfirmed(true);
+        setIsconfirms((prevConfirm) => ({ ...prevConfirm, confirmRoutine: true }));
     };
 
-    const handleStartClick = () => {
-        setIsRunning(true);
+    const handleConfirmTimeExerciseClick = () => {
+        setIsconfirms((prevConfirm) => ({ ...prevConfirm, confirmEachSet: true }));
     };
 
-    const handleStopClick = () => {
-        setIsRunning(false);
+    const handleConfirmRestClick = () => {
+        setIsconfirms((prevConfirm) => ({ ...prevConfirm, confirmRest: true }));
+    };
+
+    const toggleRunningState = () => {
+        setIsRunning(prevRunning => !prevRunning);
     };
 
     useEffect(() => {
         let timer: NodeJS.Timeout;
-
         if (isRunning) {
             timer = setInterval(() => {
-                if (seconds === 0) {
-                    if (minutes === 0) {
-                        setIsRunning(false);
-                        clearInterval(timer);
-                    } else {
-                        setMinutes(prevMinutes => prevMinutes - 1);
-                        setSeconds(59);
-                    }
-                } else {
-                    setSeconds(prevSeconds => prevSeconds - 1);
-                }
+                setTime((prevTime) => ({
+                    ...prevTime,
+                    seconds:
+                        prevTime.seconds === 0
+                            ? prevTime.minutes > 0
+                                ? 59
+                                : 0
+                            : prevTime.seconds - 1,
+                    minutes:
+                        prevTime.seconds === 0 && prevTime.minutes === 0
+                            ? 0
+                            : prevTime.minutes - (prevTime.seconds === 0 ? 1 : 0),
+                }));
             }, 1000);
         }
-
         return () => clearInterval(timer);
-    }, [isRunning, minutes, seconds]);
+    }, [isRunning, time]);
 
     return (
         <div className="flex justify-center gap-4">
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 my-auto">
                 <div className="flex items-center space-x-2">
-                    {!isConfirmed && (
+                    {confirms.confirmRoutine ? (
+                        <section>
+                            <div className="text-xl font-medium">
+                                Total time:{String(time.minutes).padStart(2, "0")}:
+                                {String(time.seconds).padStart(2, "0")}
+                            </div>
+                        </section>
+                    ) : (
                         <>
+                            <h3 className="text-xl font-medium">Total time:</h3>
                             <input
                                 type="number"
-                                value={minutes}
-                                onChange={handleMinutesChange}
+                                value={time.minutes}
+                                onChange={handleTimeChange}
                                 className="border-2 border-black rounded-md w-16 text-center"
-                                disabled={isConfirmed}
+                                disabled={confirms.confirmRoutine}
+                                name="minutes"
                             />
                             <div className="text-xl font-medium">:</div>
                             <input
                                 type="number"
-                                value={seconds}
-                                onChange={handleSecondsChange}
+                                value={time.seconds}
+                                onChange={handleTimeChange}
                                 className="border-2 border-black rounded-md w-16 text-center"
-                                disabled={isConfirmed}
+                                disabled={confirms.confirmRoutine}
+                                name="seconds"
                             />
                         </>
                     )}
                 </div>
-                <div className="flex">
-                    <div className="flex items-center space-x-2">
-                        <div className="text-xl font-medium">Total time:</div>
+
+                <div className="flex items-center space-x-2">
+                    {confirms.confirmRoutine && confirms.confirmEachSet ? (
                         <div className="text-xl font-medium">
-                            {String(minutes).padStart(2, "0")}
-                            {":"}
-                            {String(seconds).padStart(2, "0")}
+                            Each set :{String(timeRoutine.exerciseTime).padStart(2, "0")}
                         </div>
-                    </div>
+                    ) : (
+                        <>
+                            <h3 className="text-xl font-medium"> Each set :</h3>
+                            <input
+                                type="number"
+                                value={timeRoutine.exerciseTime}
+                                onChange={handleTimeRoutineChange}
+                                className="border-2 border-black rounded-md w-16 text-center"
+                                disabled={!confirms.confirmRoutine}
+                                name="exerciseTime"
+                            />
+                        </>
+                    )}
+                </div>
+
+                <div className="flex items-center space-x-2">
+                    {confirms.confirmRoutine && confirms.confirmEachSet && confirms.confirmRest ? (
+                        <div className="text-xl font-medium">
+                            Rest time :{String(timeRoutine.restTime).padStart(2, "0")}
+                        </div>
+                    ) : (
+                        <>
+                            <h3 className="text-xl font-medium"> Rest time :</h3>
+                            <input
+                                type="number"
+                                value={timeRoutine.restTime}
+                                onChange={handleTimeRoutineChange}
+                                className="border-2 border-black rounded-md w-16 text-center"
+                                disabled={!confirms.confirmEachSet}
+                                name="restTime"
+                            />
+                        </>
+                    )}
                 </div>
             </div>
-            <div className="flex flex-col my-auto">
-                {!isConfirmed && (
-                    <Btn
-                        onClick={handleConfirmClick}
-                    >
-                        Confirm
-                    </Btn>
+            <div className="my-auto">
+                {!confirms.confirmRoutine && <Btn onClick={handleConfirmClick}>Confirm Routine Time</Btn>}
+                {confirms.confirmRoutine && !confirms.confirmEachSet && (
+                    <Btn onClick={handleConfirmTimeExerciseClick}>Confirm Set Time</Btn>
                 )}
-                {isConfirmed && !isRunning && (
-                    <Btn
-                        onClick={handleStartClick}
-                    >
-                        Star
-                    </Btn>
+                 {confirms.confirmRoutine && confirms.confirmEachSet && !confirms.confirmRest &&  (
+                    <Btn onClick={handleConfirmRestClick}>Confirm Rest Time</Btn>
                 )}
-                {isRunning && (
-                    <Btn
-                        onClick={handleStopClick}
-                    >
-                        Stop
-                    </Btn>
+                {confirms.confirmRoutine && !isRunning && confirms.confirmEachSet && confirms.confirmRest &&(
+                    <Btn onClick={toggleRunningState}>Start</Btn>
                 )}
+                {isRunning && <Btn onClick={toggleRunningState}>Stop</Btn>}
             </div>
         </div>
     );

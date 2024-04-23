b8af2499ef4a82b2e7c7af50ac825895ea240a3e
LuisCF <luiscfunes521@gmail.com>
2024-04-23 11:43:13 -0600

    conditional rrendering of inputs and buttons

--- src/components/ui/ExerciseTimer.tsx
+++ src/components/ui/ExerciseTimer.tsx
@@ -8,13 +8,12 @@ interface ExerciseTimerProps {
 
 const ExerciseTimer: React.FC<ExerciseTimerProps> = ({ initialTotalTime, initialExerciseTime, initialRestTime }) => {
   const [totalTime, setTotalTime] = useState(initialTotalTime);
-  const [exerciseTime, setExerciseTime] = useState(initialExerciseTime);
-  const [restTime, setRestTime] = useState(initialRestTime);
   const [timeLeft, setTimeLeft] = useState(totalTime);
   const [isResting, setIsResting] = useState(false);
   const [isTimerStarted, setIsTimerStarted] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
-  const [editedTotalTime, setEditedTotalTime] = useState(initialTotalTime);
+  const [editedMinutes, setEditedMinutes] = useState(Math.floor(initialTotalTime / 60));
+  const [editedSeconds, setEditedSeconds] = useState(initialTotalTime % 60);
   const [editedExerciseTime, setEditedExerciseTime] = useState(initialExerciseTime);
   const [editedRestTime, setEditedRestTime] = useState(initialRestTime);
 
@@ -40,15 +39,15 @@ const ExerciseTimer: React.FC<ExerciseTimerProps> = ({ initialTotalTime, initial
     const interval = setInterval(() => {
       if (timeLeft === 0) {
         clearInterval(interval);
-      } else if (!isResting && timeLeft % (exerciseTime + restTime) === 0) {
+      } else if (!isResting && timeLeft % (initialExerciseTime + initialRestTime) === 0) {
         setIsResting(true);
-      } else if (isResting && timeLeft % (exerciseTime + restTime) === exerciseTime) {
+      } else if (isResting && timeLeft % (initialExerciseTime + initialRestTime) === initialExerciseTime) {
         setIsResting(false);
       }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [timeLeft, isResting, exerciseTime, restTime]);
+  }, [timeLeft, isResting, initialExerciseTime, initialRestTime]);
 
   const formatTime = (time: number): string => {
     const minutes = Math.floor(time / 60);
@@ -57,10 +56,9 @@ const ExerciseTimer: React.FC<ExerciseTimerProps> = ({ initialTotalTime, initial
   };
 
   const handleConfirmChanges = () => {
-    setTotalTime(editedTotalTime);
-    setExerciseTime(editedExerciseTime);
-    setRestTime(editedRestTime);
-    setTimeLeft(editedTotalTime);
+    const totalSeconds = (editedMinutes * 60) + editedSeconds;
+    setTotalTime(totalSeconds);
+    setTimeLeft(totalSeconds);
     setIsTimerStarted(true);
   };
 
@@ -68,36 +66,40 @@ const ExerciseTimer: React.FC<ExerciseTimerProps> = ({ initialTotalTime, initial
     setIsPaused(prevPaused => !prevPaused);
   };
 
-  const handleTotalTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditedTotalTime(parseInt(e.target.value));
-  };
-
-  const handleExerciseTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditedExerciseTime(parseInt(e.target.value));
-  };
-
-  const handleRestTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditedRestTime(parseInt(e.target.value));
-  };
-
   return (
-    <div className='flex flex-col gap-2 items-center'>
-      <div>
-        <label>Total Time:</label>
-        <input type="number" value={editedTotalTime} onChange={handleTotalTimeChange} className="ml-4 border-slate-600 placeholder-slate-400 w-4/12 rounded-lg border px-3 py-2 text-sm" />
+    <div className='flex flex-col gap-2 w-[300px] m-auto'>
+      {
+        isTimerStarted ? (
+          <label className='text-center'>Total Time: {formatTime(totalTime)}</label>
+        ) : (
+          <>
+            <div>
+              <label>Total Time:</label>
+              <input type="number" value={editedMinutes} onChange={e => setEditedMinutes(parseInt(e.target.value))} className="ml-2 border-slate-600 placeholder-slate-400 w-1/6 rounded-lg border px-3 py-2 text-sm" />
+              <span className='mx-auto'>:</span>
+              <input type="number" value={editedSeconds} onChange={e => setEditedSeconds(parseInt(e.target.value))} className="border-slate-600 placeholder-slate-400 w-1/6 rounded-lg border px-3 py-2 text-sm" />
+            </div>
+            <div>
+              <label>Exercise Sec:</label>
+              <input type="number" value={editedExerciseTime} onChange={e => setEditedExerciseTime(parseInt(e.target.value))} className="ml-4 border-slate-600 placeholder-slate-400 w-4/12 rounded-lg border px-3 py-2 text-sm" />
+            </div>
+            <div>
+              <label>Rest Sec:</label>
+              <input type="number" value={editedRestTime} onChange={e => setEditedRestTime(parseInt(e.target.value))} placeholder="30..." className="ml-4 border-slate-600 placeholder-slate-400 w-4/12 rounded-lg border px-3 py-2 text-sm" />
+            </div>
+          </>
+        )
+
+      }
+      {!isTimerStarted ? (
+        <button onClick={handleConfirmChanges} className='rounded-full bg-black text-white w-full p-2 hover:bg-slate-500'>Confirm Changes</button>
+      ) : (
+        <button onClick={handlePauseResume} className='rounded-full bg-black text-white w-full p-2 hover:bg-slate-500'>{isPaused ? 'Resume' : 'Pause'}</button>
+      )}
+      <div className='text-center'>
+        <h1>{isResting ? 'Resting' : 'Exercising'}</h1>
+        <p>Time Left: {formatTime(timeLeft)}</p>
       </div>
-      <div>
-        <label>Exercise Time:</label>
-        <input type="number" value={editedExerciseTime} onChange={handleExerciseTimeChange} className="ml-4 border-slate-600 placeholder-slate-400 w-4/12 rounded-lg border px-3 py-2 text-sm" />
-      </div>
-      <div>
-        <label>Rest Time:</label>
-        <input type="number" value={editedRestTime} onChange={handleRestTimeChange} placeholder="30..." className="ml-4 border-slate-600 placeholder-slate-400 w-4/12 rounded-lg border px-3 py-2 text-sm"/>
-      </div>
-      <button onClick={handleConfirmChanges} className='rounded-full bg-black text-white w-4/12 p-2 hover:bg-slate-500'>Confirm Changes</button>
-      <button onClick={handlePauseResume} className='rounded-full bg-black text-white w-4/12 p-2 hover:bg-slate-500'>{isPaused ? 'Resume' : 'Pause'}</button>
-      <h1>{isResting ? 'Resting' : 'Exercising'}</h1>
-      <p>Time Left: {formatTime(timeLeft)}</p>
     </div>
   );
 };
